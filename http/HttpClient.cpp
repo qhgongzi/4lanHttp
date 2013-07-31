a35f059984e40f5d6428a5e27ac6c867f036f828
qhgongzi <qhgongzi@gmail.com>
2013-08-01 07:30:20 +0800

    add http reader

--- http/HttpClient.cpp
+++ http/HttpClient.cpp
@@ -1,5 +1,6 @@
 #include "HttpClient.h"
 #include <iostream>
+#include "httpReader.hpp"
 
 #pragma comment( lib, "libeay32.lib" )
 #pragma comment( lib, "ssleay32.lib" )
@@ -87,103 +88,20 @@ boost::shared_ptr<ClientResult> CHttpClient::Send(CWebRequest *request)
 
 boost::shared_ptr<ClientResult> CHttpClient::readBody()
 {
-	int headSize;
+	
 	if(protocol_==1){
-		headSize=boost::asio::read_until(ssl_sock,respone_,"\r\n\r\n");
+		httpReader<tcp::socket> reader;
 	}else{
-		headSize=boost::asio::read_until(socket_,respone_,"\r\n\r\n");
+		httpReader<boost::asio::ssl::stream<boost::asio::ip::tcp::socket&>> reader
 	}
-	nHeaderLen=headSize;
-	std::istream response_stream(&respone_);
-	response_stream.unsetf(std::ios_base::skipws);//asio::streambuf 转换成istream 并且忽略空格
+	
 
-	//将数据流追加到header里
-	int readSize=respone_.size();
-
-	char * head=new char[headSize+1];
-	memset(head,0,headSize+1);
-	response_stream.read(head,headSize);
-	m_respone->header=head;
-	delete head;
-
-	int rdContentSize=readSize-m_respone->header.size();
-
-	char * cont=NULL;
-	//获取httpContent的长度
-	if(m_respone->header.find("Content-Length")!=std::string::npos)
-	{
-		std::string len=m_respone->header.substr(m_respone->header.find("Content-Length: ")+16);
-		len=len.substr(0,len.find_first_of("\r"));
-		nContentLen=atoi(len.c_str());
-
-		if(protocol_==1){
-			boost::asio::read(ssl_sock,respone_,boost::asio::transfer_at_least(nContentLen-rdContentSize));
-		}else{
-			boost::asio::read(socket_,respone_,boost::asio::transfer_at_least(nContentLen-rdContentSize));
-		}
-
-		nContentLen=respone_.size();
-		cont=new char[nContentLen+1]; //此处申请了内存，注意释放。
-		memset(cont+nContentLen,0,1);
-		response_stream.read(cont,nContentLen);
-
-	}
-	else if(m_respone->header.find("Transfer-Encoding: chunked")!=std::string::npos)
-	{
-		while (true)
-		{
-			int contSize=0;
-			if(protocol_==1){
-				contSize=boost::asio::read_until(ssl_sock,respone_,"\r\n");
-			}else{
-				contSize=boost::asio::read_until(socket_,respone_,"\r\n");
-			}
-
-			int readLen=respone_.size()-contSize;
-
-			char *chunkStr=new char[contSize]; //此处申请了内存，注意释放。
-			response_stream.read(chunkStr,contSize);
-			memset(chunkStr+contSize-2,0,2);
-			long nextReadSize=strtol(chunkStr,NULL,16);
-			if(nextReadSize==0) break;
-			delete chunkStr;
-
-			char * htmlBuf=new char[nextReadSize+2];
-
-			if(nextReadSize>readLen){
-				if(protocol_==1){
-					boost::asio::read(ssl_sock,respone_,boost::asio::transfer_at_least(nextReadSize-readLen+2));
-				}else{
-					boost::asio::read(socket_,respone_,boost::asio::transfer_at_least(nextReadSize-readLen+2));
-				}
-			}
-
-			response_stream.read(htmlBuf,nextReadSize+2);
-
-			if(cont==NULL){
-				cont=htmlBuf;
-				memset(htmlBuf+nextReadSize,0,2);
-				nContentLen=nextReadSize;
-			}else{
-				char * newCont=new char[nContentLen+nextReadSize+1];
-				memset(newCont+nContentLen+nextReadSize,0,1);
-				memcpy(newCont,cont,nContentLen);
-				memcpy(newCont+nContentLen,htmlBuf,nextReadSize);
-				delete  cont;
-				delete  htmlBuf;
-
-				cont=newCont;
-				nContentLen+=nextReadSize;
-			}
-		}
-
-	}
-
-	boost::shared_array<char> content(cont);
+	boost::shared_array<char> content=reader.read();
 	
 	m_respone->errorCode=0;
 	m_respone->msg=content;
-	m_respone->len=nContentLen;
+	m_respone->header=reader.m_header;
+	m_respone->len=header.m_bodysize;
 
 	return m_respone;
 
--- http/httpReader.hpp
+++ http/httpReader.hpp
@@ -7,8 +7,9 @@ class httpReader
 	
 
 	public:
-		bool bChunked;
-		std::string header;
+		
+		std::string m_header;
+		size_t m_bodysize;
 
 		void httpReader(Tsock sock)
 	 	{
@@ -17,7 +18,7 @@ class httpReader
 
 		}
 
-		void read()
+		std::string readHeader()
 		{
 			int headSize;
 			
@@ -33,96 +34,117 @@ class httpReader
 			char * head=new char[headSize+1];
 			memset(head,0,headSize+1);
 			response_stream.read(head,headSize);
-			m_respone->header=head;
+			std::string header=head;
 			delete head;
+			return header;
+		}
 
-			int rdContentSize=readSize-m_respone->header.size();
-
+		boost::shared_array<char> readBodyBySize()
+		{
 			char * cont=NULL;
-			//获取httpContent的长度
-			if(m_respone->header.find("Content-Length")!=std::string::npos)
-			{
-				std::string len=m_respone->header.substr(m_respone->header.find("Content-Length: ")+16);
-				len=len.substr(0,len.find_first_of("\r"));
-				nContentLen=atoi(len.c_str());
+			size_t nContentLen;
 
-				if(protocol_==1){
-					boost::asio::read(ssl_sock,respone_,boost::asio::transfer_at_least(nContentLen-rdContentSize));
+			std::string len=this->m_header.substr(this->m_header.find("Content-Length: ")+16);
+			len=len.substr(0,len.find_first_of("\r"));
+			nContentLen=atoi(len.c_str());
+
+			
+			boost::asio::read(m_sock,respone_,boost::asio::transfer_at_least(nContentLen-rdContentSize));
+			
+			nContentLen=respone_.size();
+			cont=new char[nContentLen+1]; //此处申请了内存，注意释放。
+			memset(cont+nContentLen,0,1);
+			response_stream.read(cont,nContentLen);
+
+			this->m_bodysize=nContentLen;
+
+			return boost::shared_array<char>(cont);
+
+		}
+
+		boost::shared_array<char> readBodyByChunk()
+		{
+			
+			char * cont=NULL;
+			size_t nContentLen;
+
+			while (true)
+			{
+				int contSize=0;
+				
+				contSize=boost::asio::read_until(m_sock,respone_,"\r\n");
+				
+
+				int readLen=respone_.size()-contSize;
+
+				char *chunkStr=new char[contSize]; //此处申请了内存，注意释放。
+				response_stream.read(chunkStr,contSize);
+				memset(chunkStr+contSize-2,0,2);
+				long nextReadSize=strtol(chunkStr,NULL,16);
+				delete chunkStr;
+				if(nextReadSize==0) break;
+				
+
+				char * htmlBuf=new char[nextReadSize+2];
+
+				if(nextReadSize>readLen){	
+					boost::asio::read(m_sock,respone_,boost::asio::transfer_at_least(nextReadSize-readLen+2));
+				}
+
+				response_stream.read(htmlBuf,nextReadSize+2);
+
+				if(cont==NULL){
+					cont=htmlBuf;
+					memset(htmlBuf+nextReadSize,0,2);
+					nContentLen=nextReadSize;
 				}else{
-					boost::asio::read(socket_,respone_,boost::asio::transfer_at_least(nContentLen-rdContentSize));
+					char * newCont=new char[nContentLen+nextReadSize+1];
+					memset(newCont+nContentLen+nextReadSize,0,1);
+					memcpy(newCont,cont,nContentLen);
+					memcpy(newCont+nContentLen,htmlBuf,nextReadSize);
+					delete  cont;
+					delete  htmlBuf;
+
+					cont=newCont;
+					nContentLen+=nextReadSize;
 				}
-
-				nContentLen=respone_.size();
-				cont=new char[nContentLen+1]; //此处申请了内存，注意释放。
-				memset(cont+nContentLen,0,1);
-				response_stream.read(cont,nContentLen);
-
 			}
-			else if(m_respone->header.find("Transfer-Encoding: chunked")!=std::string::npos)
+
+			this->m_bodysize=nContentLen;
+
+			return boost::shared_array<char>(cont);
+
+		}
+
+
+		boost::shared_array<char> read()
+		{
+			
+			this->m_header=this->readHeader();
+
+			int rdContentSize=readSize-this->m_header.size();
+
+			
+			//获取httpContent的长度
+			if(this->m_header.find("Content-Length")!=std::string::npos)
 			{
-				while (true)
-				{
-					int contSize=0;
-					if(protocol_==1){
-						contSize=boost::asio::read_until(ssl_sock,respone_,"\r\n");
-					}else{
-						contSize=boost::asio::read_until(socket_,respone_,"\r\n");
-					}
-
-					int readLen=respone_.size()-contSize;
-
-					char *chunkStr=new char[contSize]; //此处申请了内存，注意释放。
-					response_stream.read(chunkStr,contSize);
-					memset(chunkStr+contSize-2,0,2);
-					long nextReadSize=strtol(chunkStr,NULL,16);
-					if(nextReadSize==0) break;
-					delete chunkStr;
-
-					char * htmlBuf=new char[nextReadSize+2];
-
-					if(nextReadSize>readLen){
-						if(protocol_==1){
-							boost::asio::read(ssl_sock,respone_,boost::asio::transfer_at_least(nextReadSize-readLen+2));
-						}else{
-							boost::asio::read(socket_,respone_,boost::asio::transfer_at_least(nextReadSize-readLen+2));
-						}
-					}
-
-					response_stream.read(htmlBuf,nextReadSize+2);
-
-					if(cont==NULL){
-						cont=htmlBuf;
-						memset(htmlBuf+nextReadSize,0,2);
-						nContentLen=nextReadSize;
-					}else{
-						char * newCont=new char[nContentLen+nextReadSize+1];
-						memset(newCont+nContentLen+nextReadSize,0,1);
-						memcpy(newCont,cont,nContentLen);
-						memcpy(newCont+nContentLen,htmlBuf,nextReadSize);
-						delete  cont;
-						delete  htmlBuf;
-
-						cont=newCont;
-						nContentLen+=nextReadSize;
-					}
-				}
-
+				
+				return this->readBodyBySize();
 			}
-
-			boost::shared_array<char> content(cont);
-	
-
-			if(header.find("chunked")!=header.npos)
+			else if(this->m_header.find("Transfer-Encoding: chunked")!=std::string::npos)
 			{
-				this->bChunked=true;
-			}else{
-				this->bChunked=false;
+				
+				return this->readBodyByChunk()
 			}
+
+			 content(cont);
+
 		}
 
 
 
 	private:
 		Tsock m_sock;
+		boost::asio::streambuf respone_;
 
 }
